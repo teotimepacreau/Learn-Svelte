c3ea5335750104659ea084815ec0dbdff7123edb
Teotime Pacreau <teotime.pac@outlook.fr>
2024-01-12 17:07:38 +0100

    build(animation)

--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,17 +1,19 @@
 <script>
-    import FeedbackItem from "./FeedbackItem.svelte";
-    export let feedback = []
-
+  import FeedbackItem from "./FeedbackItem.svelte";
+  import { fade, scale } from "svelte/transition";
+  export let feedback = [];
 </script>
 
 <div class="feedback-list">
-{#each feedback as fb (fb.id)}
-    <FeedbackItem item={fb} on:delete-feedback/>
-{/each}
+  {#each feedback as fb (fb.id)}
+    <div in:scale out:fade="{{duration: 500}}">
+      <FeedbackItem item={fb} on:delete-feedback />
+    </div>
+  {/each}
 </div>
 
 <style>
-    .feedback-list {
-        margin-top: calc(2*var(--rythme));
-    }
+  .feedback-list {
+    margin-top: calc(2 * var(--rythme));
+  }
 </style>
